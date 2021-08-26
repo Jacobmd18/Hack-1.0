cd331d8b56e1280af1ec1ddf1f305a3272afe5bc
Jacobmd18 <jdalton12@huskers.unl.edu>
2021-08-26 23:56:46 +0000

    Added the code to print name and major

---
+++ Hack 1.c
@@ -0,0 +1,19 @@
+/**
+ *
+ * Author: Enter your Name
+ * Date: Enter the date
+ *
+ * Hack 1.0
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+    printf("Enter your name \n");
+    printf("Enter your major");
+
+
+    return 0;
+}
